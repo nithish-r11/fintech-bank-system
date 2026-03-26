27b7dccc38fec73fbe99e00cf5d7738c98e843ed
Murugananthan <murugananthan2006@gmail.com>
2026-03-26 15:50:34 +0530

    UI update + KYC fields + dashboard improvements stable

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
 
     ROLE_CHOICES = (
@@ -10,8 +11,15 @@ class User(AbstractUser):
     )
 
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
-    failed_login_attempts = models.IntegerField(default=0)
     account_locked = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.username
+    # ⭐ login security fields
+    failed_login_attempts = models.IntegerField(default=0)
+
+    # ⭐ KYC + profile fields
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    aadhaar = models.CharField(max_length=20, blank=True, null=True)
+    pan = models.CharField(max_length=20, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    pincode = models.CharField(max_length=10, blank=True, null=True)
+    kyc_verified = models.BooleanField(default=False)
